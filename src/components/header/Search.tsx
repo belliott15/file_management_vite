f800831aafc37817357714c691967b02065a32cf
Beau Elliott <beauelliott@Beaus-MacBook-Pro.local>
2024-03-28 13:34:16 -0700

    added fontAwesome for icons and updated header bar

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -1,11 +1,16 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Search = () => {
   return (
-    <>
-      <form>
-        <input placeholder="Search" className="rounded-md" />
-        <button></button>
+    <div className="bg-white rounded-md p-1">
+      <form onSubmit={() => console.log("search")}>
+        <input placeholder="Search" className="focus:outline-none" />
+        <button>
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
       </form>
-    </>
+    </div>
   );
 };
 
