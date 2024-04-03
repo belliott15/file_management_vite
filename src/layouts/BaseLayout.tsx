eb882fe3de7d0778266218ed87c5d19712f89e7e
Beau Elliott <beauelliott@Beaus-MacBook-Pro.local>
2024-04-02 23:20:47 -0700

    building out routing

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,9 +1,20 @@
-import Header from "../components/header/Header";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "../pages/HomePage/Homepage";
+import Login from "../pages/AuthPages/Login/Login";
+import Register from "../pages/AuthPages/Register/Register";
+import { NavigationComponent } from "../components/HomePageComponent";
+import Footer from "../components/Footer/Footer";
 
 const BaseLayout = () => {
   return (
     <div>
-      <Header />
+      <NavigationComponent />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Footer />
     </div>
   );
 };
